4a0f3cc53ec2e3c2d4bdaceda0b8127008d2a221
Rokas V. <100084190+rokvaz@users.noreply.github.com>
2024-10-23 21:46:36 +0300

    deploy

--- js/demo.js
+++ js/demo.js
@@ -6,23 +6,9 @@
 // This can be used to set the Particles Effects. Check README for more details!
 document.addEventListener('DOMContentLoaded', function () {
   particleground(document.getElementById('particles'), {
-    dotColor: '#5cbdaa',
-    lineColor: '#5cbdaa'
+    dotColor: '#6DD546',
+    lineColor: '#4AA926'
   });
   var intro = document.getElementById('intro');
   intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
 }, false);
-
-
-/*
-// jQuery plugin example:
-$(document).ready(function() {
-  $('#particles').particleground({
-    dotColor: '#5cbdaa',
-    lineColor: '#5cbdaa'
-  });
-  $('.intro').css({
-    'margin-top': -($('.intro').height() / 2)
-  });
-});
-*/
